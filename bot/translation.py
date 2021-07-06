3f0e233d98bf11feb859d594347e1511385a7d46
RonaldoA123 <86909673+RonaldoA123@users.noreply.github.com>
2021-07-06 20:41:33 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,12 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>Hi bro😁
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+you can't add me to your group 😂
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@malayalammoviesme
 </i>"""    
     
     HELP_TEXT = """
@@ -43,7 +43,7 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@malayalammoviesme
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
